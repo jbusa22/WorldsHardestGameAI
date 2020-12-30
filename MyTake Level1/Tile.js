b974f9796155ea3df566eddde1bd21f6f89431c2
jbusa <jbusa@umich.edu>
2020-12-29 20:46:20 -0500

    Add click and drag to level select

--- MyTake Level1/Tile.js
+++ MyTake Level1/Tile.js
@@ -2,15 +2,38 @@ class Tile {
   constructor(x, y) {
     this.centerPoint = createVector(x,y)
     this.pixelPos = createVector(x*tileSize+xoff, y*tileSize + yoff)
+    this.toggled = false
+    this.dragChanged = false
+    if ((this.centerPoint.x +this.centerPoint.y) % 2 ==0) {
+      this.red = 247
+      this.unSelectedRed = 247
+      this.green = 247
+      this.blue = 255
+    } else {
+      this.red = 230
+      this.unSelectedRed = 230
+      this.green = 230
+      this.blue = 255
+    }
   }
   show(){
-    if ((this.centerPoint.x +this.centerPoint.y) % 2 ==0) {
-     fill(247,247,255);
-    } else {
-      fill(230,230,255);
-    }
+    fill(this.red,this.green,this.blue);
     noStroke()
     rect(this.pixelPos.x, this.pixelPos.y, tileSize, tileSize)
+  }
+  drag(turnOn) {
+    if (!this.dragChanged && this.toggled != turnOn) {
+      this.toggle()
+      this.dragChanged = true
+    }
+  }
+  clearDrag() {
+    this.dragChanged = false
+  }
+  toggle() {
+      this.toggled = !this.toggled
+      this.red = this.toggled ? 0 : this.unSelectedRed
+      return this.toggled
   }
 
 }
--- MyTake Level1/sketch.js
+++ MyTake Level1/sketch.js
@@ -1,29 +1,72 @@
 var tiles = []
 var tileSize = 50;
-var xoff = 200
-var yoff = 100
+var xoff = 0
+var yoff = 0
 
-
+var rows = 10
+var cols = 20
+var canvasWidth = tileSize * cols
+var canvasHeight = tileSize * rows
+console.log(canvasHeight, canvasWidth)
 function setup() {
-  createCanvas(1280, 720)
+  createCanvas(canvasWidth, canvasHeight)
 
-  for (var i = 0; i< 22; i++) {
+  for (var i = 0; i< rows; i++) {
     tiles[i] = [];
-    for (var j = 0; j< 10; j++) {
-      tiles[i][j] = new Tile(i, j);
+    for (var j = 0; j< cols; j++) {
+      tiles[i][j] = new Tile(j, i);
     }
-  }  
+  }
 }
 
 function draw() {
   background(255,255,255)
   drawTiles()
 }
+var iClicked = 0
+var jClicked = 0
+var turnOn = false
+function mousePressed() {
+  if (mouseX > xoff && mouseX < xoff + canvasWidth 
+      && mouseY > yoff && mouseY < yoff + canvasHeight) {
+    iClicked = Math.floor(mouseY / tileSize)
+    jClicked = Math.floor(mouseX / tileSize)
+    turnOn = tiles[iClicked][jClicked].toggle();
+  }
+}
+
+var dragging = false
+function mouseDragged() {
+  if (mouseX > xoff && mouseX < xoff + canvasWidth 
+      && mouseY > yoff && mouseY < yoff + canvasHeight) {
+    dragging = true
+    let i = Math.floor(mouseY / tileSize)
+    let j = Math.floor(mouseX / tileSize)
+    if (i != iClicked || j != jClicked) {
+      tiles[i][j].drag(turnOn);
+    }
+  } 
+}
+
+function mouseReleased() {
+  if (dragging) {
+    dragging = false
+    clearDragging()
+  }
+}
+
+function clearDragging() {
+  for (var i = 0; i< rows; i++) {
+    for (var j = 0; j< cols; j++) {
+      tiles[i][j].clearDrag();
+    }
+  }
+}
 
 // show all tiles
 function drawTiles() {
-  for (var i = 0; i< tiles.length; i++) {
-    for (var j = 0; j< tiles[0].length; j++) {
+  for (var i = 0; i< rows; i++) {
+    for (var j = 0; j< cols; j++) {
       tiles[i][j].show();
     }
   }
